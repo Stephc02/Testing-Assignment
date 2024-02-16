6b00765fee85aac1b09f7097c95d09fe128bc8fd
Stephc02 <103242667+Stephc02@users.noreply.github.com>
2024-02-16 02:37:40 +0100

    Update WebsiteModelTest.java

--- cucumber-demo/src/test/java/Website/WebsiteModelTest.java
+++ cucumber-demo/src/test/java/Website/WebsiteModelTest.java
@@ -7,29 +7,7 @@ import org.junit.Test;
 
 import java.util.Random;
 
- /*Transition and guard for adding to cart - old way
-    public boolean addToCartGuard() {
-        return currentState == WebsiteState.BROWSING_PRODUCTS;
-    }
-    @Action
-    public void addToCart() {
-        currentState = WebsiteState.ADDING_PRODUCT_TO_CART;
-        // Additional actions to interact with the website
-    }
 
- OLD WAY TO CHECKOUT
-    Transition and guard for checking out
-    public boolean checkoutGuard() {
-        return currentState == WebsiteState.ADDING_PRODUCT_TO_CART;
-    }
-
-    @Action
-    public void checkout() {
-        currentState = WebsiteState.CHECKING_OUT;
-        // Additional actions to interact with the website
-    }
-
-    */
 
 public class WebsiteModelTest implements FsmModel {
     // Define state variables
@@ -132,44 +110,6 @@ public class WebsiteModelTest implements FsmModel {
         currentState = WebsiteState.CHECKING_OUT;
         // Additional actions to interact with the website
     }
-
-/*
-    NOT_LOGGED_IN,
-    LOGGED_IN,
-    BROWSING_PRODUCTS,
-
-    SEARCH,  have to do
-    VIEWING_PRODUCT_DETAILS, have to do
-
-    ADDING_PRODUCT_TO_CART,
-
-    VIEW_CART,  have to do
-    REMOVING_PRODUCT_FROM_CART, have to do
-    CHECKING_OUT
-
-Transitions:
-- Click on Category (from Not Logged in to Browsing Products)
-  - Not Logged in -> Browsing Products (NotLoggedIn - true, BrowsingProduct - true)
-
-- Write in Search (from Browsing products to Search)
-   - Browsing products -> Search   (BrowsingProduct - false, ProductSearched - true)
-
-- Click on Product (from Search to Viewing Product Details)
-   - Search -> Viewing Product Details   (ProductSearched = true, ProductDetailsViewed = true)
-
-- Click on Add button (from Viewing Product Details to Adding Product to Cart)
-  - Viewing Product Details -> Adding Product to Cart (ProductDetailsViewed = true, ProductInCart - true)
-
-- Click on Remove button (from View Cart to Removing Product from Cart)
-  - View Cart -> Removing Product from Cart (ProductInCart - true, ProductInCart - false)
-
-- Checkout (from View Cart to Checking out)
-   - View Cart -> Checking out (ProductInCart - true, CheckoutInitiated - true)
-
-  */
-
-
-
 
 
 
